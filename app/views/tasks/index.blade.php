164ff8586c833bc1d084aa283de76b5fa285b670
Jalil Wahdatehagh <j.wahdatehagh@gmail.com>
2014-01-19 21:11:09 +0100

    bootstrap cdn and user migrations

--- app/views/tasks/index.blade.php
+++ app/views/tasks/index.blade.php
@@ -2,7 +2,9 @@
 
 @section('content')
 	<h1>All Tasks</h1>
-	@foreach ($tasks as $task)
-		<li>{{ link_to("tasks/$task->id", $task->title) }}</li>
-	@endforeach
+	<ul class="list-group">
+		@foreach ($tasks as $task)
+			<li class="list-group-item">{{ link_to("tasks/$task->id", $task->title) }}</li>
+		@endforeach
+	</ul>
 @stop
