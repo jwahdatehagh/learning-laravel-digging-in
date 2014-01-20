dab968968e72d19cc916d39e637c8fbbfad9ae90
Jalil Wahdatehagh <j.wahdatehagh@gmail.com>
2014-01-20 20:27:55 +0100

    lesson 3 done - user-task relationships, new routing

--- app/models/Task.php
+++ app/models/Task.php
@@ -1,5 +1,8 @@
 <?php
 
 class Task extends Eloquent {
-	
+	public function user() 
+	{
+		return $this->blelongsTo('User');
+	}
 }
--- app/routes.php
+++ app/routes.php
@@ -1,4 +1,25 @@
 <?php
 
-Route::get('tasks', 'TasksController@index');
+Route::get('/', ['as' => 'home', 'uses' => 'TasksController@index']);
 Route::get('tasks/{id}', 'TasksController@show')->where('id', '\d+');
+
+Route::get('{username}/tasks', function($username)
+{
+	// find user by username from users table
+	$user = User::whereUsername($username)->first();
+	
+	// get all tasks associated with that user
+	// return Task::whereUserId($user->id)->get(); --> instead assign relationships...
+	return $user->tasks;
+	// return them
+});
+
+Route::get('{username}/tasks/{id}', function($username, $id) 
+{
+	// find the task by its id
+	$user = User::whereUsername($username)->first();
+	$task = $user->tasks()->findOrFail($id);
+
+	// and load a view to display it
+	return View::make('tasks.show', compact('task'));
+});
